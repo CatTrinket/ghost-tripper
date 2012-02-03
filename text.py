ae3a5f420437062145bcc051af4527f08ea04578
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2012-02-03 07:32:47 -0500

    Ditch lz_decompress() in favour of magical's beautiful code.

--- text.py
+++ text.py
@@ -1,52 +1,8 @@
 from struct import unpack
 from sys import argv
 
+from lzss3 import decompress
 from tables import *
-
-def lz_decompress(bytes):
-    # Copy-pasted straight out of bw_animations.py; pretty unreadable but I
-    # guess it works!
-    lz_type = bytes[0]
-    size = unpack('<L', bytes[:4])[0] >> 8
-    bytes = bytes[4:]
-    data = bytearray()
-    while bytes and len(data) < size:
-        type_flags = bytes[0]
-        bytes = bytes[1:]
-        for bit in reversed(range(8)):
-            if len(data) >= size:
-                break
-            if type_flags >> bit & 1:
-                indicator = bytes[0] >> 4
-                if lz_type == 0x10:
-                    thing, = unpack('>H', bytes[:2])
-                    num = (thing >> 12) + 3
-                    distance = thing & 0xfff
-                    bytes = bytes[2:]
-                elif indicator == 0:
-                    thing, = unpack('>L', b'\x00' + bytes[:3])
-                    num = (thing >> 12) + 0x11
-                    distance = thing & 0xfff
-                    bytes = bytes[3:]
-                elif indicator == 1:
-                    thing, = unpack('>L', bytes[:4])
-                    num = (thing >> 12 & 0xffff) + 0x111
-                    distance = thing & 0xfff
-                    bytes = bytes[4:]
-                else:
-                    thing, = unpack('>H', bytes[:2])
-                    num = (thing >> 12) + 1
-                    distance = thing & 0xfff
-                    bytes = bytes[2:]
-
-                for byte_read in range(num):
-                    data.append(data[-(distance + 1)])
-            else:
-                try:
-                 data.append(bytes[0])
-                 bytes = bytes[1:]
-                except IndexError: print (data, len(data), size, bytes)
-    return data
 
 def read_char():
     global data
@@ -54,7 +10,10 @@ def read_char():
     data = data[2:]
     return char
 
-data = lz_decompress(open(argv[1], 'rb').read())
+data = decompress(open(argv[1], 'rb'))
+
+#print(data[0xe9c:])
+#exit()
 
 while data:
     char = read_char()
