18d3fd2babe38c1670d2306fc35d7260e5a6d7b6
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2012-02-21 22:58:09 -0500

    Rip a greyscale Capcom logo, almost.

--- sprites.py
+++ sprites.py
@@ -2,6 +2,7 @@ from io import BytesIO
 from struct import unpack
 
 from formats import parse_cpac
+from lzss3 import decompress
 
 def untile(data, width, height):
     """Width and height are tiles.  Needs a toooon of improvement."""
@@ -16,8 +17,11 @@ def untile(data, width, height):
         #  2  3  6  7
         #  8  9 12 13
         # 10 11 14 15
-        tile_x = tile % 8 // 4 * 2 + tile % 2
-        tile_y = tile // 8 * 2 + tile // 2 % 2
+        # tile_x = tile % 8 // 4 * 2 + tile % 2
+        # tile_y = tile // 8 * 2 + tile // 2 % 2
+
+        tile_x = tile % width
+        tile_y = tile // width
 
         for pixel_y in range(8):
             y = tile_y * 8 + pixel_y
@@ -25,7 +29,10 @@ def untile(data, width, height):
             for pixel_x in range(8):
                 x = tile_x * 8 + pixel_x
 
-                pixels[y][x] = data.pop(0)
+                try:
+                    pixels[y][x] = data.pop(0)
+                except OverflowError:
+                    pixels[y][x] = 0
 
     return pixels
 
@@ -40,20 +47,20 @@ for n, section in enumerate(data_sections):
     output.write(section)
 
 
-# Rip a few tiles of... something to a PGM.
-data = BytesIO(data_sections[4])
-data.seek(0xd3300 - 512)
+# Rip the Capcop logo???
+data = BytesIO(data_sections[2])
+data.seek(0xa00)  # Pointer found at 0x14
 
-pixels = untile(bytearray(data.read(1024)), 4, 4)
+pixels = decompress(data)
+pixels = untile(pixels, 24, 8)
 
-with open('/tmp/tiles.pgm', 'w') as output:
-    # PGM header
+with open('/tmp/logo.ppm', 'w') as output:
+    # PPM header
     output.write(
         'P2\n'
-        '32 32\n'
+        '192 64\n'
         '255\n'
     )
 
     for row in pixels:
-        output.write(' '.join(str(pixel) for pixel in row))
-        output.write('\n')
+        print(*row, file=output)
