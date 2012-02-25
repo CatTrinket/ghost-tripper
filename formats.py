a8abe4b330d09bc548a26f5292a4ed640f477a86
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2012-02-25 00:33:09 -0500

    I mean NotImplementedError, not NotImplemented, oops.

--- formats.py
+++ formats.py
@@ -48,7 +48,7 @@ class Screen():
             for pixel_num, pixel in enumerate(tiles[tile.tile]):
                 # XXX Transforming will go here once I need it
                 if tile.transformation != 0:
-                    raise NotImplemented('NTFS tile transformation')
+                    raise NotImplementedError('NTFS tile transformation')
 
                 # The pixel's location within the entire image
                 x = pixel_num % 8 + tile_x * 8
